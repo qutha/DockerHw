6723bf3d2ff62ef93a828122d686334b9b931810
Basil <vasyakuzmin1@yandex.ru>
2024-12-06 21:08:46 +0300

    добавлен проект

---
+++ DockerHwApi.Server/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello from Docker!");
+app.MapGet("/rad/{deg:double}", (double deg) => Math.PI * deg / 180.0);
+
+app.Run();
+
+// xUnit
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class Program;
---
+++ DockerHwApi.Tests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DockerHwApi.Tests;
+
+public class Tests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task GetRoot_ReturnsHelloMessage()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Hello from Docker!", content);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(180, Math.PI)]
+    [InlineData(360, 2 * Math.PI)]
+    public async Task ConvertDegreesToRadians_ReturnsExpectedResult(double degrees, double expectedRadians)
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"/rad/{degrees}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(expectedRadians.ToString(CultureInfo.InvariantCulture), content);
+    }
+}
